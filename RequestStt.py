56151ffb917591a88eeda4a2c34e3b89b63fbbdd
KDH <xmflzhqxj12@naver.com>
2025-04-29 15:38:51 +0900

    공유변수 설정

--- RequestStt.py
+++ RequestStt.py
@@ -6,10 +6,8 @@ import wave
 
 import requests
 
-from util import load_hw_device
-
-FILE_PATH = "/home/pi/my_project/stt.wav"
-API_URL = "http://3.34.179.85:8000/api/stt"
+from config import BASE_URL, WAV_PATH
+from util import load_hw_device, suppress_alsa_errors
 
 SILENCE_THRESHOLD = 100 # 말을 안할 때 진폭
 SILENCE_DURATION = 3  # 말 멈추고 3초 뒤 종료
@@ -18,6 +16,7 @@ RATE = 44100
 CHANNELS = 1
 
 def record_audio():
+    
     try:
         print("음성 녹음중...")
 
@@ -25,43 +24,48 @@ def record_audio():
         cmd = [
             "arecord",
             "-D", device,
-            "-f", "cd",
+            "-f", "S16_LE",
             "-r", str(RATE),
             "-c", str(CHANNELS),
             "-t", "raw"
         ]
 
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         frames = []
         silence_start = None
 
-        while True:
-            data = process.stdout.read(CHUNK_SIZE)
-            if not data:
-                break
+        with suppress_alsa_errors():
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 
-            frames.append(data)
-            rms = audioop.rms(data, 2)
-
-            if rms < SILENCE_THRESHOLD:
-                if silence_start is None:
-                    silence_start = time.time()
-                elif time.time() - silence_start > SILENCE_DURATION:
-                    print("말이 멈췄다고 판단됨. 녹음 종료.")
+            while True:
+                data = process.stdout.read(CHUNK_SIZE)
+                if not data:
                     break
-            else:
-                silence_start = None
 
-        process.terminate()
+                frames.append(data)
+                rms = audioop.rms(data, 2)
 
-        # wav 파일 저장
-        with wave.open(FILE_PATH, 'wb') as wf:
+                if rms < SILENCE_THRESHOLD:
+                    if silence_start is None:
+                        silence_start = time.time()
+                    elif time.time() - silence_start > SILENCE_DURATION:
+                        print("말이 멈췄다고 판단됨. 녹음 종료.")
+                        break
+                else:
+                    silence_start = None
+
+            process.terminate()
+            process.wait()
+
+        if not frames:
+            print("녹음 데이터가 없습니다.")
+            return False
+
+        with wave.open(WAV_PATH, 'wb') as wf:
             wf.setnchannels(CHANNELS)
             wf.setsampwidth(2)
             wf.setframerate(RATE)
             wf.writeframes(b''.join(frames))
 
-        print("녹음 파일 저장 완료:", FILE_PATH)
         return True
 
     except Exception as e:
@@ -69,17 +73,23 @@ def record_audio():
         return False
     
 def upload_stt():
+    url = f"{BASE_URL}/api/stt"
+    
     if record_audio():
-        if os.path.exists(FILE_PATH):
+        if os.path.exists(WAV_PATH):
             try:
-                with open(FILE_PATH, "rb") as f:
-                    response = requests.post(API_URL, files={"audio": f})
-                    return response.text.strip()
+                with open(WAV_PATH, "rb") as f:
+                    response = requests.post(url, files={"audio": f})
+                    if response.status_code == 200:
+                        return response.text.strip()
+                    else:
+                        print(f"STT 서버 응답 실패: {response.status_code}")
+                        return ""
             except Exception as e:
-                print(f"API 요청 실패: {e}")
+                print(f"STT 서버 요청 실패: {e}")
                 return ""
         else:
-            print("저장된 녹음 파일이 없습니다.")
+            print("녹음 파일 없음")
             return ""
     else:
         print("녹음 실패")
