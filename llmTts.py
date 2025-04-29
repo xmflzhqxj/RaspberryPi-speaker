515ba1787662b039690b2039794afe1ff37e9e36
KDH <xmflzhqxj12@naver.com>
2025-04-29 21:40:22 +0900

    taking_medicine 추가

--- llmTts.py
+++ llmTts.py
@@ -13,10 +13,7 @@ def conversation_and_check(responsetype="", schedule_id=None, user_id=None):
         print(f"녹음 파일이 존재하지 않거나 너무 작습니다: {WAV_PATH}")
         return "" if responsetype != "check_medicine" else False
 
-    if responsetype == "check_medicine":
-        real_schedule_id = schedule_id
-    else:
-        real_schedule_id = DUMMY_ID
+    real_schedule_id = schedule_id if responsetype == "check_medicine" else DUMMY_ID
 
     files = {"audio": open(WAV_PATH, "rb")}
     params = {
@@ -26,7 +23,6 @@ def conversation_and_check(responsetype="", schedule_id=None, user_id=None):
     }
 
     try:
-        print(f"conversation_and_check 실행 시작 (responsetype: {responsetype})")
         response = requests.post(url, files=files, params=params)
         if response.status_code == 200:
             result = response.json()
@@ -63,3 +59,52 @@ def conversation_and_check(responsetype="", schedule_id=None, user_id=None):
         files["audio"].close()
 
     return False if responsetype == "check_medicine" else ""
+
+def post_taking_medicine(schedule_id, user_id):
+    url = f"{BASE_URL}/api/test2"
+
+    if not os.path.exists(WAV_PATH) or os.path.getsize(WAV_PATH) < 1000:
+        print(f"고정 음성파일이 존재하지 않거나 너무 작습니다: {WAV_PATH}")
+        return False
+
+    files = {"audio": open(WAV_PATH, "rb")}
+    params = {
+        "userId": user_id,
+        "scheduleId": schedule_id,
+        "responsetype": "taking_medicine_time"
+    }
+
+    try:
+        response = requests.post(url, files=files, params=params)
+        if response.status_code == 200:
+            result = response.json()
+            text = result.get("message", "")
+            print(text)
+            audio_url = result.get("file_url", "")
+
+            if audio_url:
+                audio_data = requests.get(audio_url)
+                if audio_data.status_code == 200:
+                    with open(LLM_VOICE_PATH, "wb") as f:
+                        f.write(audio_data.content)
+
+                    if os.path.exists(LLM_VOICE_PATH):
+                        subprocess.run(["mpg123", LLM_VOICE_PATH], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    else:
+                        print("taking_medicine_time 음성 파일이 존재하지 않습니다.")
+                else:
+                    print(f"음성 다운로드 실패: {audio_data.status_code}")
+            else:
+                print("서버 응답에 file_url 없음")
+            return True
+
+        else:
+            print(f"서버 응답 실패: {response.status_code} - {response.text}")
+
+    except Exception as e:
+        print(f"post_taking_medicine 중 오류: {e}")
+
+    finally:
+        files["audio"].close()
+
+    return False
