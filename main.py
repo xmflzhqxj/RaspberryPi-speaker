4dff021a4328c362ecd2147bfa52b4c9a6fb88a5
KDH <xmflzhqxj12@naver.com>
2025-04-30 18:03:20 +0900

    위치 수정

--- main.py
+++ main.py
@@ -15,16 +15,17 @@ from WakeWord import wakeWord_forever
 
 if __name__ == "__main__":
     
-    initialize_settings()
-    
     if wait_for_microphone():
         time.sleep(2)
         auto_save_mic()
         auto_save_speaker()
     else:
         print("마이크를 찾을 수 없습니다.")
-        
+     
     wait_for_network()
+    
+    initialize_settings()   
+    
     
     scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
     scheduler_thread.start()
