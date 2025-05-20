5355e807eebf6c23fa43d8b53c6a2362990934f5
KDH <xmflzhqxj12@naver.com>
2025-05-20 15:15:24 +0900

    프로토타입

--- config.py
+++ config.py
@@ -3,11 +3,14 @@ BASE_URL = "http://3.34.179.85:8000"
 
 # 사용자 ID
 USER_ID = 1
+FE_USER_ID = 3
+
 WAV_PATH = "/home/pi/my_project/stt.wav"
 LLM_VOICE_PATH = "/home/pi/my_project/llm_answer.mp3"
+DUMMY_PATH = "/home/pi/my_project/test.wav"
 # 복약 리마인더 설정
 DOSAGE_TIME = 1
 DOSAGE_COUNT = 3
-MEAL_TIME = 3
+MEAL_TIME = 2
 INDUCE_TIME = 1
 DUMMY_ID = -1
--- global_state.py
+++ global_state.py
@@ -1,5 +1,6 @@
-from collections import deque
 import threading
+from collections import deque
+
 # 공통으로 사용할 전역 alert 큐
 pending_alerts = deque()
 mic_lock = threading.Lock()
