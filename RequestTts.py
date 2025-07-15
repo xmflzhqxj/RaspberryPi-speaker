d203018942d9f4e9c64c9959b1850967b223b0bb
xmflzhqxj <xmflzhqxj12@naver.com>
2025-07-15 22:45:25 +0900

    RequestTts.py
    Text To Speech

--- RequestTts.py
+++ RequestTts.py
@@ -5,11 +5,22 @@ from config import BASE_URL
 from gpio_controller import GPIOController
 from util import safe_play
 
-gpio = GPIOController(refresh_callback=lambda: None, skip_callback=lambda: None)
+gpio = GPIOController(refresh_callback=lambda: None, skip_callback=lambda: None) # 시각적 LED 표시
+
+# api에서 진행되는 TTS 코드
+# from gtts import gTTS 구글 gTTS 라이브러리
+# from playsound import playsound
+
+# def text_to_voice(text): // .mp3 파일 실행
+#     tts = gTTS(text=text, lang='ko')
+#     fileName = "/home/pi/my_project/tts.mp3"
+#     tts.save(fileName)
+#     playsound(fileName)
+#     print("TTS complete")
 
 # TTS 수행 함수
 def text_to_voice(text):
-    url = f"{BASE_URL}/api/tts"
+    url = f"{BASE_URL}/api/tts" # api에 요청
     gpio.set_mode("llmtts")
     try:
         payload = {"text": text}
