54e4b7fd769e1158e4b0f6f772fd13accb3d8ab7
xmflzhqxj <xmflzhqxj12@naver.com>
2025-07-15 22:40:47 +0900

    RequestStt.py
    Speech To Text

--- RequestStt.py
+++ RequestStt.py
@@ -13,13 +13,13 @@ from util import load_hw_device
 
 gpio = GPIOController(refresh_callback=lambda: None, skip_callback=lambda: None)
 
-SILENCE_THRESHOLD = 1600  
-SILENCE_DURATION = 3
+SILENCE_THRESHOLD = 1600 # 입력되는 소음의 진폭
+SILENCE_DURATION = 3 # 말 종료 판단 시간
 CHUNK_SIZE = 1024
-RATE = 44100
+RATE = 44100 # 음성 sampling frequency
 CHANNELS = 1
 
-def record_audio():
+def record_audio(): # 음성 녹음 함수
     with mic_lock:
         try:
             print("음성 녹음중...")
@@ -52,10 +52,10 @@ def record_audio():
                 frames.append(data)
                 rms = audioop.rms(data, 2)
 
-                if rms < SILENCE_THRESHOLD:
+                if rms < SILENCE_THRESHOLD: # 목소리가 안 들리는 경우
                     if silence_start is None:
                         silence_start = time.time()
-                    elif time.time() - silence_start > SILENCE_DURATION:
+                    elif time.time() - silence_start > SILENCE_DURATION: # 정해진 시간동안 목소리 입력 안 된 경우
                         break
                 else:
                     silence_start = None
@@ -90,7 +90,15 @@ def record_audio():
             gpio.set_mode("error")
             return False
 
-def upload_stt():
+# import speech_recognition as sr 구글 STT api 사용
+# r= sr.Recognizer()
+# audio_file = sr.AudioFile('/home/pi/my_project/test.wav')
+# with audio_file as source:
+# 	audio = r.record(source)
+
+# print(r.recognize_google(audio,language='ko-KR')) 
+
+def upload_stt(): # STT 함수 
     url = f"{BASE_URL}/api/stt"
     
     if not record_audio():
@@ -113,7 +121,7 @@ def upload_stt():
             response = requests.post(url, files={"audio": f})
             if response.status_code == 200:
                 print(f"STT : {response.text.strip()}")
-                return response.text.strip()
+                return response.text.strip() # STT 결과 text 리턴
             else:
                 print(f"STT 서버 응답 실패: {response.status_code}")
                 gpio.set_mode("error")
